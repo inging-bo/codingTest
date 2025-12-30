5d8e5d10b22b3be103eb228a8a2a68a1e4eae44b
inging-bo <thervv@kakao.com>
2025-12-30 19:08:54 +0900

    [level 0] Title: 문자열 출력하기, Time: 44.42 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    console.log(str);
+});
+
