562c75f58909d7f2a1b64942da77e77675bf5d87
inging-bo <thervv@kakao.com>
2026-01-28 23:58:50 +0900

    [level 2] Title: 주차 요금 계산, Time: 12.15 ms, Memory: 34.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -0,0 +1,50 @@
+function solution(fees, records) {
+    let [basicTime, basicFee, unitTime, unitFee] = fees
+    
+    let history = {}
+    let parkingTimes = {}
+    
+    for (let i = 0; i < records.length; i++) {
+        let [time, carNum, move] = records[i].split(" ")
+        let [hourStr, minuteStr] = time.split(":")
+        let hour = Number(hourStr)
+        let minute = Number(minuteStr)
+        
+        
+        if (!isNaN(history[carNum])) {
+            console.log(history[carNum])
+            let curTime = hour * 60 + minute
+            let inTime = history[carNum]
+            parkingTimes[carNum] += curTime - inTime
+            history[carNum] = "re"
+        } else {
+            
+            if (history[carNum] === "re") {
+                history[carNum] = hour * 60 + minute        
+            } else {
+                history[carNum] = hour * 60 + minute    
+                parkingTimes[carNum] = 0    
+            }
+        }
+    }
+    
+    
+    for (let [key, value] of Object.entries(parkingTimes)) {
+        if (history[key] !== "re") {
+            value += 1439 - history[key]
+        }
+        if (value > basicTime) {
+            parkingTimes[key] = basicFee + Math.ceil((value - basicTime) / unitTime) * unitFee    
+        } else {
+            parkingTimes[key] = basicFee
+        }
+    }
+    
+    let answer = []
+    
+    let sort = Object.entries(parkingTimes).sort(([key,value],[keyB, valueB]) => key - keyB)
+    for (let [key, value] of sort) {
+        answer.push(value)
+    }
+    return answer;
+}
